64b5423a9bdd3dd0c62727639ad646d774c96186
1066 <git@1066.io>
2015-09-18 13:04:32 -0700

    added objects

--- robots_enum.py
+++ robots_enum.py
@@ -6,54 +6,68 @@ import wget
 import re
 import urllib
 
-url = raw_input('Enter Robots URL' ' e.g. "www.awesomehack.com/robots.txt"\n')
-if url[0] == 'w':
-    print('Please add http:// next time!')
-    url = 'http://'+url
-    first_slash = url.index('/')
-    remove_slashes = url[first_slash+2:]
-    last_slash = remove_slashes.index('/')
-    truncate_url = remove_slashes[:last_slash]
-else:
-    first_slash = url.index('/')
-    remove_slashes = url[first_slash+2:]
-    last_slash = remove_slashes.index('/')
-    truncate_url = remove_slashes[:last_slash]
-with requests.Session() as s:
-    s = s.get(url)
-    url_text = s.text
+class Url():
+    def __init__(self):
+        self.url = raw_input('Enter Robots URL' ' e.g. "www.awesomehack.com/robots.txt"\n')
+        text_url = ''
+        truncate_url = ''
+        self.robots = set()
+        self.url_count = 1
+        self.url_range = []
+        self.final_url_list = []
+        self.robots_dict = {}
 
-regex = re.compile('(Allow: )(.+)')
-repl = '\\1'
-text = regex.sub(repl, url_text)
+    def get_url(self):
+        if self.url[0] == 'w':
+            print('Please add http:// next time!')
+    	    self.url = 'http://'+self.url
+        return self.url
 
-# Find all instances of the Disallow pages
-regex = re.compile('(Disallow: )(/.+)')
-repl = '\\2'
-text = regex.sub(repl, text)
-robots = re.findall('^/.+', text, flags=re.MULTILINE)
-# Get rid of any Duplicate robots files
-remove_duplicate = list(set(robots))
 
-url_count = len(remove_duplicate)
-url_count = set(range(1, url_count +1))
-new_url_list = []
+    def remove_slashes(self):
+        first_slash = self.url.index('/')
+        remove_slashes = self.url[first_slash+2:]
+        last_slash = remove_slashes.index('/')
+        self.truncate_url = remove_slashes[:last_slash]
+        return self.truncate_url
 
-for path in remove_duplicate:
-    new_url = truncate_url + path
-    new_url = 'http://' + new_url
-    new_url_list.append(new_url)
 
-new_url_list = [x.encode('UTF8') for x in new_url_list]
-robots_dict = dict(zip(url_count, new_url_list))
+    def make_request(self):
+        with requests.Session() as s:
+            s = s.get(self.url)
+            self.url_text = s.text
+            return self.url_text
+
+    def remove_allow(self):
+        url_text = self.url_text
+        regex = re.compile('(Allow: )(.+)')
+        repl = '\\1'
+        text = regex.sub(repl, url_text)
+        # Find all instances of the Disallow pages
+        regex = re.compile('(Disallow: )(/.+)')
+        repl = '\\2'
+        text = regex.sub(repl, text)
+        robots = re.findall('^/.+', text, flags=re.MULTILINE)
+        # Get rid of any Duplicate robots files
+        self.robots = robots
+        self.url_count = len(self.robots)
+        self.url_range = set(range(1, self.url_count +1))
+        for path in self.robots:
+            new_url = self.truncate_url + path
+            new_url = 'http://' + new_url
+            self.final_url_list.append(new_url)
+        self.final_url_list = [x.encode('UTF8') for x in self.final_url_list]
+        self.robots_dict = dict(zip(self.url_range, self.final_url_list))
+
 
 
 list_cache = {}
 bad_urls = []
+
 def list(choice):
     if choice == 'list':
         good_urls = []
-        for item in robots_dict.items():
+        for item in u.robots_dict.items():
             r = requests.get(item[1])
             if r.status_code == 200:
                 good_urls.append(item[1])
@@ -91,7 +105,7 @@ def download_all():
     good_urls = []
     good_url_number = []
 
-    for item in robots_dict.items():
+    for item in u.robots_dict.items():
         r = requests.get(item[1])
         if r.status_code == 200:
             good_urls.append(item[1])
@@ -117,7 +131,7 @@ rejected_dict_cache = {}
 def bad_requests():
     rejected_codes = []
     rejected_urls = []
-    for item in robots_dict.items():
+    for item in u.robots_dict.items():
         r = requests.get(item[1])
         if r.status_code != 200:
             rejected_codes.append(r.status_code)
@@ -131,12 +145,33 @@ def see_bad():
         print
         print 'Enter corresponding number with URL to download or type "download all" to download every "good" url'
         print
-        get_bad = raw_input('Some directories/files from the robots file on ' + url + ' have received "bad" error codes. Would you like to see what they are? y/n\n')
+        get_bad = raw_input('Some directories/files from the robots file on ' + u.url + ' have received "bad" error codes. Would you like to see what they are? y/n\n')
         if get_bad == 'y':
-            bad_requests()
-print
-print'Looks like there are ' + str(len(remove_duplicate)) +  ' Disallow: pages on ' + url
-print 'OPTIONS: Help for help, list to see all robot pages available, download all to download all robots pages and exit() to exit' \
+            try:
+                if bool(rejected_dict_cache) == False:
+                    bad_requests()
+
+
+                elif bool(rejected_dict_cache) == True:
+                    for item in rejected_dict_cache.items():
+                        print item
+
+            except:print 'No Bad Requests Found'
+
+u = Url()
+u.get_url()
+u.remove_slashes()
+u.make_request()
+u.remove_allow()
+
+def show_disallow():
+    print
+    print'Looks like there are ' + str(u.url_count) +  ' Disallow: pages on ' + u.url
+    print 'OPTIONS: Help for help, list to see all robot pages available, download all to download all robots pages and exit() to exit' \
+
+show_disallow()
+
+
 
 while True:
     choice = raw_input('>')
@@ -149,11 +184,17 @@ while True:
         continue
 
     if choice == 'bad':
-        if bool(rejected_dict_cache) == True:
-            for item in rejected_dict_cache.items():
-               print item
-        else:
-            print 'There may not be any bad responses. Try running list first.'
+        try:
+            if bool(rejected_dict_cache) == False:
+                bad_requests()
+
+
+            elif bool(rejected_dict_cache) == True:
+                for item in rejected_dict_cache.items():
+                    print item
+
+        except:print 'No Bad Requests Found'
+
         continue
 
     if choice == 'download all':
@@ -171,5 +212,18 @@ while True:
     if choice.isdigit():
         list(choice)
 
-        continue
+    if choice == 'new url':
+        u = Url()
+        u.get_url()
+        u.remove_slashes()
+        u.make_request()
+        u.remove_allow()
+        show_disallow()
+
+
+    continue
+
+
+
+
 
